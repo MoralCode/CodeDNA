dbebf0631307dde982e1bfba2c93db274ac5fbaf
Adrian Edwards <adrian@adriancedwards.com>
2025-03-03 21:10:02 -0500

    refactor it into a function

--- code-dna.go
+++ code-dna.go
@@ -9,6 +9,30 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
+func getLineageID(repo *git.Repository) string {
+	// ... retrieving the HEAD reference
+	ref, err := repo.Head()
+	CheckIfError(err)
+
+	// ... retrieves the commit history
+	// since := time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
+	// until := time.Date(2019, 7, 30, 0, 0, 0, 0, time.UTC)
+	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	// , Since: &since, Until: &until
+	CheckIfError(err)
+
+	lineageID := ""
+
+	// ... just iterates over the commits, printing it
+	err = cIter.ForEach(func(c *object.Commit) error {
+		// fmt.Printf("%v (%T)", string(c.Hash.String()[0]), string(c.Hash.String()[0]))
+		lineageID += string(c.Hash.String()[0])
+		return nil
+	})
+	CheckIfError(err)
+	return lineageID
+}
+
 func main() {
 	CheckArgs("<path>")
 	path := os.Args[1]
@@ -19,24 +43,5 @@ func main() {
 
 	// Length of the HEAD history
 	// Info("git rev-list HEAD --count")
-
-	// ... retrieving the HEAD reference
-	ref, err := r.Head()
-	CheckIfError(err)
-
-	// ... retrieves the commit history
-	// since := time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
-	// until := time.Date(2019, 7, 30, 0, 0, 0, 0, time.UTC)
-	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
-	// , Since: &since, Until: &until
-	CheckIfError(err)
-
-	// ... just iterates over the commits, printing it
-	err = cIter.ForEach(func(c *object.Commit) error {
-		// fmt.Printf("%v (%T)", string(c.Hash.String()[0]), string(c.Hash.String()[0]))
-		fmt.Print(string(c.Hash.String()[0]))
-
-		return nil
-	})
-	CheckIfError(err)
+	fmt.Println(getLineageID(r))
 }
