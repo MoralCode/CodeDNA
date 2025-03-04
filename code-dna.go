4b27a8524b4d41bf7224327d58d526c7987193a6
Adrian Edwards <adrian@adriancedwards.com>
2025-03-03 23:39:31 -0500

    reverse the lineageID

--- code-dna.go
+++ code-dna.go
@@ -12,6 +12,15 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
+// https://stackoverflow.com/a/10030772/
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 func getLineageID(repo *git.Repository) string {
 	// ... retrieving the HEAD reference
 	ref, err := repo.Head()
@@ -33,7 +42,7 @@ func getLineageID(repo *git.Repository) string {
 		return nil
 	})
 	CheckIfError(err)
-	return lineageID
+	return Reverse(lineageID)
 }
 
 // isValidUrl tests a string to determine if it is a well-structured url or not.
