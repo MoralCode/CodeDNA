a3ba1e433395c08b37ed1d1f7b6986479049d962
Adrian Edwards <adrian@adriancedwards.com>
2025-03-23 17:34:09 -0400

    move cache functions to utility file

--- code-dna.go
+++ code-dna.go
@@ -183,52 +183,6 @@ func getLongestPrefix(str1 string, str2 string) string {
 		return getLongestPrefix(left1, left2)
 	}
 }
-
-func check_cache(path string) string {
-	pathparts := strings.Split(path, "/")
-	reponame := pathparts[len(pathparts)-1]
-	cacheFilename := "./" + reponame
-
-	if !strings.HasSuffix(cacheFilename, ".txt") {
-		cacheFilename += ".txt"
-	}
-
-	inputfile, err := os.Stat(path)
-	if err != nil {
-
-		// Checking if the given file exists or not
-		// Using IsNotExist() function
-		if os.IsNotExist(err) {
-			log.Fatal("File not Found !!")
-		}
-	}
-	mode := inputfile.Mode()
-
-	if mode.IsRegular() {
-
-		// do file stuff
-		data, err := os.ReadFile(cacheFilename)
-		check(err)
-		return string(data)
-	}
-
-	log.Fatal(errors.New("Cachepath shouldnt be a directory"))
-}
-
-func write_cache(path string, lineageID string) {
-	pathparts := strings.Split(path, "/")
-	reponame := pathparts[len(pathparts)-1]
-	cacheFilename := "./" + reponame
-
-	if !strings.HasSuffix(cacheFilename, ".txt") {
-		cacheFilename += ".txt"
-	}
-
-	d1 := []byte(lineageID)
-
-	err := os.WriteFile(cacheFilename, d1, 0644)
-	check(err)
-}
 func main() {
 	CheckArgs("<path> <path2>")
 	path1 := os.Args[1]
